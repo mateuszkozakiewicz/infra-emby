b343339fa7021ea8140c94b6ea5f8276d0b113f2
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2022-09-02 22:57:53 +0200

    add vault master key

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -1,10 +1,24 @@
 locals {
-  vault_display_name = "emby-vault"
-  vault_vault_type   = "DEFAULT"
+  vault_display_name      = "emby-vault"
+  vault_vault_type        = "DEFAULT"
+  key_display_name        = "emby-vault-master-key"
+  key_key_shape_algorithm = "AES"
+  key_key_shape_length    = 32
 }
 
 resource "oci_kms_vault" "emby_vault" {
   compartment_id = var.compartment_ocid
   display_name   = local.vault_display_name
   vault_type     = local.vault_vault_type
+}
+
+resource "oci_kms_key" "emby_vault_master_key" {
+  compartment_id = var.compartment_ocid
+  display_name   = local.key_display_name
+  key_shape {
+    algorithm = local.key_key_shape_algorithm
+    length    = local.key_key_shape_length
+  }
+  management_endpoint = oci_kms_vault.emby_vault.management_endpoint
+  protection_mode     = "HSM"
 }
