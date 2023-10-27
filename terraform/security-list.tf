88a9b81ebf717921f15b63bdc778120860a62425
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2023-10-27 10:33:48 +0200

    add pritunl port rules

--- terraform/security-list.tf
+++ terraform/security-list.tf
@@ -1,9 +1,12 @@
 locals {
   tcp_protocol       = "6"
+  udp_protocol       = "17"
   anywhere           = "0.0.0.0/0"
   ssh_port           = "22"
   http_port          = "80"
   https_port         = "443"
+  openvpn_port       = "1194"
+  wireguard_port     = "1195"
   postgres_port      = "5432"
   security_list_name = "security-list"
 }
@@ -55,6 +58,26 @@ resource "oci_core_security_list" "security_list" {
     }
 
     protocol = local.tcp_protocol
+    source   = var.cluster_ip
+  }
+
+  ingress_security_rules {
+    udp_options  {
+      min = local.openvpn_port
+      max = local.openvpn_port
+    }
+
+    protocol = local.udp_protocol
+    source   = var.cluster_ip
+  }
+
+  ingress_security_rules {
+    udp_options  {
+      min = local.wireguard_port
+      max = local.wireguard_port
+    }
+
+    protocol = local.udp_protocol
     source   = var.cluster_ip
   }
 }
