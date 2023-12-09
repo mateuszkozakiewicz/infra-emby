7ee6e40caa8e2d0ae134b3f1ba74d2f15186aec6
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2023-12-09 22:53:30 +0100

    allow cloudflare

--- terraform/security-list.tf
+++ terraform/security-list.tf
@@ -9,6 +9,30 @@ locals {
   wireguard_port     = "1195"
   postgres_port      = "5432"
   security_list_name = "security-list"
+  cloudflare_ips = [
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/13",
+    "104.24.0.0/14",
+    "172.64.0.0/13",
+    "131.0.72.0/22",
+    "2400:cb00::/32",
+    "2606:4700::/32",
+    "2803:f800::/32",
+    "2405:b500::/32",
+    "2405:8100::/32",
+    "2a06:98c0::/29",
+    "2c0f:f248::/32"
+  ]
 }
 
 resource "oci_core_security_list" "security_list" {
@@ -62,7 +86,7 @@ resource "oci_core_security_list" "security_list" {
   }
 
   ingress_security_rules {
-    udp_options  {
+    udp_options {
       min = local.openvpn_port
       max = local.openvpn_port
     }
@@ -72,12 +96,26 @@ resource "oci_core_security_list" "security_list" {
   }
 
   ingress_security_rules {
-    udp_options  {
+    udp_options {
       min = local.wireguard_port
       max = local.wireguard_port
     }
 
     protocol = local.udp_protocol
     source   = var.cluster_ip
+  }
+
+  dynamic "ingress_security_rules" {
+    for_each = local.cloudflare_ips
+
+    content {
+      tcp_options {
+        min = local.http_port
+        max = local.http_port
+      }
+
+      protocol = local.tcp_protocol
+      source   = ingress_security_rules.value
+    }
   }
 }
