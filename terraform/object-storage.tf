0bca42e83d2ae0d0db56e9cb9de858c3f3cc746f
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2026-04-19 19:33:02 +0200

    export keys

--- terraform/object-storage.tf
+++ terraform/object-storage.tf
@@ -1,13 +1,21 @@
 locals {
   bucket_name = "bucket"
-  namespace   = "frau7ohucd40"
+}
+
+data "oci_objectstorage_namespace" "emby" {
+  compartment_id = var.compartment_ocid
+}
+
+data "oci_identity_compartment" "emby" {
+  #Required
+  id = var.compartment_ocid
 }
 
 resource "oci_objectstorage_bucket" "emby" {
   #Required
   compartment_id = var.compartment_ocid
   name           = local.bucket_name
-  namespace      = local.namespace
+  namespace      = data.oci_objectstorage_namespace.emby.namespace
 
   #Optional
   access_type           = "NoPublicAccess"
@@ -15,4 +23,36 @@ resource "oci_objectstorage_bucket" "emby" {
   object_events_enabled = false
   storage_tier          = "Standard"
   versioning            = "Disabled"
+}
+
+resource "oci_identity_user" "bucket_identity" {
+  compartment_id = var.tenancy_ocid
+  name           = "emby-bucket-identity-user"
+  description    = "Service account for emby object storage"
+}
+
+resource "oci_identity_group" "bucket_identity_group" {
+  compartment_id = var.tenancy_ocid
+  name           = "emby-bucket-identity-group"
+  description    = "Group for emby object storage service account"
+}
+
+resource "oci_identity_user_group_membership" "bucket_identity_membership" {
+  group_id = oci_identity_group.bucket_identity_group.id
+  user_id  = oci_identity_user.bucket_identity.id
+}
+
+resource "oci_identity_policy" "bucket_identity_policy" {
+  compartment_id = var.compartment_ocid
+  name           = "emby-bucket-identity-policy"
+  description    = "Allow service account to access object storage bucket"
+  statements = [
+    "Allow group emby-bucket-identity-group to manage objects in compartment ${data.oci_identity_compartment.emby.name} where target.bucket.name='${oci_objectstorage_bucket.emby.name}'",
+    "Allow group emby-bucket-identity-group to read buckets in compartment ${data.oci_identity_compartment.emby.name}",
+  ]
+}
+
+resource "oci_identity_customer_secret_key" "bucket_identity_secret_key" {
+  user_id      = oci_identity_user.bucket_identity.id
+  display_name = "emby-bucket-identity-s3-key"
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,20 @@ output "instance_public_ip" {
 output "vault_ocid" {
   value = oci_kms_vault.emby_vault.id
 }
+
+output "public_id" {
+  value = oci_core_public_ip.public.ip_address
+}
+
+output "bucket_access_key" {
+  value = oci_identity_customer_secret_key.bucket_identity_secret_key.id
+}
+
+output "bucket_secret_key" {
+  value     = oci_identity_customer_secret_key.bucket_identity_secret_key.key
+  sensitive = true
+}
+
+output "bucket_s3_endpoint" {
+  value = "https://${data.oci_objectstorage_namespace.emby.namespace}.compat.objectstorage.${var.region}.oraclecloud.com"
+}
